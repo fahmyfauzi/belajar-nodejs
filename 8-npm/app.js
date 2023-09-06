d272674ad1ddd23c2c172105ea083785db1bedae
fahmyfauzi <fahmyfauzii@gmail.com>
2023-09-06 14:32:10 +0700

    menggunakan package npm

--- 8-npm/app.js
+++ 8-npm/app.js
@@ -1,1 +1,18 @@
-console.log('halo dunia');
+// console.log('halo dunia');
+const validator = require('validator');
+const chalk = require('chalk');
+
+//----npm validator
+// text = validator.isEmail('fahmy@gmail.com'); //true
+
+// phone = validator.isMobilePhone('092118418130', 'id-ID');
+// number = validator.isNumeric('092118418a130');
+
+// console.log(number);
+
+//----npm chalk
+
+const nama = 'Fahmy';
+// console.log(chalk.blue.bgGreen.italic(`Hello ${nama}`));
+const pesan = chalk `halo {green.bgBlue.bold ${nama}} fauzi {bgRed.bold.italic alvianda}`;
+console.log(pesan);
